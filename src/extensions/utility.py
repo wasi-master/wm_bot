wasi-master/wm_bot

commit 02e33de093ce9d70368bbace2797d9d45081e32f

Fix Grammar

src/extensions/utility.py
@@ -30,10 +30,10 @@ class Utility(commands.Cog):
     async def unshorten(self, ctx, url: str):
         """Got a shortened link? bit.ly? use this command to un shorten the link!
 
-        Does not work for website that do not redirect you to the long url directly.
+        Does not work for websites that do not redirect you to the long url directly.
         """
         async with self.bot.session.get(url, allow_redirects=True) as resp:
-            if resp.url == url: # XXX: there may be a better way to do this
+ammar            if resp.url == url: # XXX: there may be a better way to do this
                 return await ctx.send("The url didn't redirect me to any website :(")
             result_url = resp.url
 

commit 8814dda17cf658fcc524b710fff6c2989d37679c

Fix a bug introduced in https://github.com/wasi-master/wm_bot/commit/02e33de093ce9d70368bbace2797d9d45081e32f

src/extensions/utility.py
@@ -33,7 +33,7 @@ class Utility(commands.Cog):
         Does not work for websites that do not redirect you to the long url directly.
         """
         async with self.bot.session.get(url, allow_redirects=True) as resp:
-ammar            if resp.url == url: # XXX: there may be a better way to do this
+            if resp.url == url: # XXX: there may be a better way to do this
                 return await ctx.send("The url didn't redirect me to any website :(")
             result_url = resp.url
 